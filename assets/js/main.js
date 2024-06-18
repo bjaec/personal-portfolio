b5d6a96b6a9ec3088166f5d9b75ba7cdb102739d
Bryant <bjc61@duke.edu>
2024-06-17 20:08:36 -0700

    added dark mode and added CS 216 project to website

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,5 +85,34 @@ const contactForm = document.getElementById('contact-form'),
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+//Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//We get the current them that the interfacehas by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line': 'ri-sun-line'
+
+//validate if the user previously chose a topic 
+if (selectedTheme){
+    //if validation is fulfilled, ask waht the issue was to know if we activated or deactivated the dark 
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add': 'remove'](iconTheme)
+}
+
+//activate / deactivate the theme manually with the button 
+themeButton.addEventListener('click', ()=> {
+    //add or remove the dark / icon theme 
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    //save theme and current icon that the user chose 
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
